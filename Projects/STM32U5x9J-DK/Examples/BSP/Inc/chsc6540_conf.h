8886cc1cfddfcd96b7750ef30bf79b278baa3edb
NEP-ENG\claude.coache <claude.coache@neptronic.com>
2025-01-08 07:54:11 -0500

    Add chsc6540 configuration

---
+++ Projects/STM32U5x9J-DK/Examples/BSP/Inc/chsc6540_conf.h
@@ -0,0 +1,39 @@
+/**
+  ******************************************************************************
+  * @file    chsc6540_conf.h
+  * @author  MCD Application Team
+  * @brief   This file contains specific configuration for the
+  *          chsc6540.c that can be modified by user.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef CHSC6540_CONF_H
+#define CHSC6540_CONF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+/* Macros --------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+#define CHSC6540_MAX_X_LENGTH                  480U
+#define CHSC6540_MAX_Y_LENGTH                  854U
+  
+#ifdef __cplusplus
+}
+#endif
+#endif /* CHSC6540_CONF_H */
+
